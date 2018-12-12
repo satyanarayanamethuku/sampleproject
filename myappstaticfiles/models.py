c381b1cc4702812805f8936934fec629d74ace89
satya <satya7259@gmail.com>
2018-12-12 14:45:21 +0530

    my first

---
+++ myappstaticfiles/models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+class Contact(models.Model):
+    firstname=models.CharField(max_length=20)
+    email=models.EmailField(max_length=50)
+    message=models.CharField(max_length=1000)
+    time=models.DateTimeField(auto_now_add=True)
+
+
+
+class Carrers(models.Model):
+    job_title=models.CharField(max_length=30,editable=True)
+    experience_reqiured=models.IntegerField(default='0',editable=True)
+    req_skills=models.CharField(max_length=100,default='skills required',null=False,editable=True)
+    job_description=models.TextField(max_length=5000,null=True,editable=True)
+    contact_no=models.CharField(max_length=10,editable=True,default='0000000')
+    updated_time = models.DateTimeField(auto_now_add=False,auto_now=True)
+    job_id=models.CharField(max_length=8,default=0)
+    job_location=models.CharField(max_length=30,default='location')
+
+
+# Create your models here.
